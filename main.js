f9994a83763e032f4b21602283a1acfa8a172b45
TobiasPucci <62162914+TobiasPucci@users.noreply.github.com>
2024-02-18 20:18:54 -0300

    keyboard shortcuts

--- main.js
+++ main.js
@@ -1,17 +1,30 @@
 const label = document.getElementById("label");
 
+/**
+* Removes the last character from the display.
+*/
 function delChar(){
     label.textContent = label.textContent.slice(0,label.textContent.length-1);
 }
 
-function resetLabel(){
+/**
+ * Clears all values from the display.
+ */
+function resetLabel(){ 
     label.textContent = "";
 }
 
+/**
+ * Adds a character to the display at the end. 
+ * @param {string} character A string to add to the display.
+ */
 function addChar(character){
     label.textContent += character;
 }
 
+/**
+ * Splits up the string of the display into different subequations based on the position of the parenthesis and updates it with the result if possible or with a message displaying wether the syntax was invalid.
+ */
 function parseEquation(){
     if (isEquationValid()){    
         let result = label.textContent.replace(',','.').split(/(\([^\(\)]*\))/g).map(calculateThird);
@@ -28,6 +41,11 @@ function parseEquation(){
     }
 }
 
+/**
+ * Removes the parenthesis from the equation and calculate its value if it starts and ends with parentheses. Otherwise returns the equations as is.
+ * @param {*} equation an equation to solve.
+ * @returns the result of the equation or the equation if it did not start and end with parentheses.
+ */
 function calculateThird(equation){
     if (equation.startsWith("(") && equation.endsWith(")")){
         return calculateSecond(equation.slice(1,equation.length-1))
@@ -36,6 +54,11 @@ function calculateThird(equation){
     }
 }
 
+/**
+ * Splits the equation into an array, using the minus and or plus sign to split it. If the minus is preceded by a multiplication or division sign, it is ignored. Afterwards, it solves the addition and subtraction of the equation and returns the result.
+ * @param {*} equation an equation to solve.
+ * @returns the result of the equation or the same string received if it is not an addition or subtraction.
+ */
 function calculateSecond(equation){
     let result = equation.split((/((?<![/*])-|\+)/)).map(calculateFirst)
     while (result.length>1){
@@ -47,6 +70,11 @@ function calculateSecond(equation){
     return result[0]
 }
 
+/**
+ * Splits the equation into an array using the multiplication and division sign to split it and returns the result of the mentioned opperations. It also includes the minus sign if it is preceded by either of the signs mentioned before.
+ * @param {*} equation an equation to solve.
+ * @returns the result of the equation or the same string received if it is not a multiplication or division.
+ */
 function calculateFirst(equation){
     let result = equation.split(/([*/]|\*-|\/-)/)
     while (result.length>1){
@@ -58,6 +86,10 @@ function calculateFirst(equation){
     return result[0]
 }
 
+/**
+ * Reads the display tag and returns true or false if the number of open/close parentheses match and if it does not include any combinations of symbols that this calculator can not interpret correctly.
+ * @returns true if the equation can be calculated, false if the syntax can not be interpreted.
+ */
 function isEquationValid(){
     let openParenthesis = label.textContent.match(/\(/g)
     let closeParenthesis = label.textContent.match(/\)/g)
